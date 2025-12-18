72bbd73cd947a1fbd0a0f70b6e4c443c6257caa0
bonheurNE07 <bonheurndezenc@gmail.com>
2025-12-18 11:40:58 +0200

    chore(blog): implemented the blog app signal to pre-generate the slug and to calculate the read time

---
+++ apps/blog/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Post
+from .services import generate_blog_slug, calculate_reading_time
+
+
+@receiver(pre_save, sender=Post)
+def pre_save_post_receiver(sender, instance, **kwargs):
+    """
+    Signal to automatically generate slug and calculate reading time
+    before saving a Post instance.
+    """
+    if not instance.slug:
+        instance.slug = generate_blog_slug(instance.title)
+
+    if instance.content:
+        instance.reading_time = calculate_reading_time(instance.content)
