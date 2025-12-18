8ce9097246e561dae61778b302ef07fd77dbd095
bonheurNE07 <bonheurndezenc@gmail.com>
2025-12-18 11:32:51 +0200

    chore(blog): implemented models and register the model to the django admin site

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,4 +1,21 @@
-# TODO implement in future
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Post model.
+    """
+
+    list_display = ("title", "author", "status", "created_at")
+    list_filter = ("status", "author", "created_at")
+    search_fields = ("title", "summary", "content")
+    ordering = ("-created_at",)
+    prepopulated_fields = {"slug": ("title",)}
+
+    fieldsets = (
+        (None, {"fields": ("title", "slug", "author", "summary", "content")}),
+        ("Status", {"fields": ("status",)}),
+        ("Media", {"fields": ("image",)}),
+    )
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,4 +1,63 @@
-# TODO implement in future
-# from django.db import models
+import uuid
+from django.db import models
+from django.conf import settings
+from django.core.validators import MaxLengthValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# hexagonal/Ports: import shared mixins
+from apps.core.models import TimeStampedModel, SoftDeleteModel
+from .managers import PostManager
+
+
+class StatusChoices(models.TextChoices):
+    DRAFT = "draft", _("Draft")
+    PUBLISHED = "published", _("Published")
+    ARCHIVED = "archived", _("Archived")
+
+
+class Post(TimeStampedModel, SoftDeleteModel):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="blog_posts",
+        verbose_name=_("Author"),
+    )
+
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    summary = models.TextField(
+        help_text="Short overview of the article",
+        validators=[MaxLengthValidator(500)],
+    )
+    content = models.TextField(
+        help_text="Main content of the blog\
+            post in markdown format"
+    )
+
+    image = models.ImageField(upload_to="portfolio/", null=True, blank=True)
+
+    status = models.CharField(
+        max_length=2,
+        choices=StatusChoices.choices,
+        default=StatusChoices.DRAFT,
+        verbose_name=_("Status"),
+    )
+
+    objects = PostManager()
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+
+        indexes = [
+            models.Index(fields=["status"]),
+            models.Index(fields=["slug"]),
+        ]
+
+    def __str__(self):
+        return self.title
