be27b90d6f9eccd839dd28554652a1a70cd3eb85
bonheurNE07 <bonheurndezenc@gmail.com>
2025-12-22 22:23:18 +0200

    chore: implemented some improvement on project app urls and views

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, ProjectFeaturedViewSet
 
 app_name = "projects"
 
 router = DefaultRouter()
+router.register(r"featured", ProjectFeaturedViewSet, basename="featured-projects")
 router.register(r"", ProjectViewSet, basename="projects")
 
 urlpatterns = router.urls
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from apps.core.pagination import CustomPagination
 from apps.core.permissions import IsAdminOrReadOnly
 from .models import Project
 from .serializers import ProjectSerializer
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(ReadOnlyModelViewSet):
     """
     API endopoint for projects.
     -Public: Read-only access to PUBLISHED projects.
@@ -22,3 +22,22 @@ class ProjectViewSet(viewsets.ModelViewSet):
             return Project.objects.all()
 
         return Project.objects.published()
+
+
+class ProjectFeaturedViewSet(ReadOnlyModelViewSet):
+    """
+    API endopoint for projects.
+    -Public: Read-only access to PUBLISHED projects.
+    -Admin: Full access.
+    """
+
+    serializer_class = ProjectSerializer
+    pagination_class = CustomPagination
+    permission_classes = [IsAdminOrReadOnly]
+    lookup_field = "slug"
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Project.objects.all()
+
+        return Project.objects.published().featured()
