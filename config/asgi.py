87d72a342de00b4ad594678ad26816895aab7014
Bonheur Ndeze <bonheurndezenc@gmail.com>
2025-12-29 21:54:11 +0200

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    config("DJANGO_SETTINGS_MODULE", default="config.settings.local"),
+    config("DJANGO_SETTINGS_MODULE", default="config.settings.production"),
 )
 
 application = get_asgi_application()
