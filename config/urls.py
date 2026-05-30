5131486c8b5218656989f82c284c80413ab521d0
bonheurNE07 <bonheurndezenc@gmail.com>
2026-05-31 01:29:00 +0200

    Configure global urls configuration with central routing schemas

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -33,3 +35,7 @@ urlpatterns = [
     # Contact API URLs
     path("api/contact/", include("apps.contact.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
